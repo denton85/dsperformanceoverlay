e759194db81ff258190051ecf357cfa261db3e34
denton85 <dentonwholmgren@gmail.com>
2025-12-02 17:06:10 +0200

    Added UI to example

--- main.gd
+++ main.gd
@@ -2,6 +2,16 @@ extends Node3D
 
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("hide"):
-		for i in get_children():
+		toggle_boxes()
+
+func toggle_boxes() -> void:
+	for i in get_children():
 			if i.is_in_group("Boxes"):
 				i.visible = !i.visible
+
+func _on_show_hide_pressed() -> void:
+	toggle_boxes()
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
