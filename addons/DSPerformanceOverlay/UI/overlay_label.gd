2e0bf31333ef63d8ade2ce410ce0b55915b657c2
denton85 <dentonwholmgren@gmail.com>
2025-12-10 00:05:17 +0200

    Update to make it update less fast, can do a timer too

--- addons/DSPerformanceOverlay/UI/overlay_label.gd
+++ addons/DSPerformanceOverlay/UI/overlay_label.gd
@@ -3,9 +3,15 @@ extends Label
 
 var perf_monitor: Performance.Monitor
 var front_label: String
+var tick: int = 0
 
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
-	text = front_label + ": " + str(Performance.get_monitor(perf_monitor)) + " |"
+	if tick < 59:
+		tick += 1
+		return
+	else:
+		text = front_label + ": " + str(Performance.get_monitor(perf_monitor)) + " |"
+		tick = 0
